eef4a368e44ff79371f7ab1046dd8ce08839c13f
Suraj Rathor <rathor.suraj0203@gmail.com>
2025-08-21 00:13:57 +0530

    style

--- client/src/pages/SignupPage.js
+++ client/src/pages/SignupPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import {useNavigate } from 'react-router-dom';
 import { useSignUpEmailPassword } from '@nhost/react';
 import Transition from '../components/Transition';
 import '../App.css';
